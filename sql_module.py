1de6145a616c4ce22f96b7bed583a6edb08ee966
Jacob Campbell <jacobcampbell11@byui.edu>
2023-01-10 16:21:54 -0700

    Created new Repo for swl project

---
+++ sql_module.py
@@ -0,0 +1,17 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(database):
+    conn = None
+    try:
+        conn = sqlite3.connect(database)
+        print(sqlite3.version)
+
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+if __name__ == '__main__':
+    create_connection('database.db')
